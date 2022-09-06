240be0c211e7cfcd539b9f7e886c813a9c0b75c4
Abhinandan Wadhwa <abhinandanwadhwa5@gmail.com>
2022-09-06 15:13:17 +0530

    Complement of Base 10 Integer

--- LeetCode/1009.cpp
+++ LeetCode/1009.cpp
@@ -1,1 +1,36 @@
-// DOUBT
+// DOUBT (DONE (BY ME ONLY))
+
+class Solution {
+public:
+    int bitwiseComplement(int n) {
+        if (n == 0) {
+            return 1;
+        }
+        else {
+            int count = 0;
+        
+            int temp = n;
+            
+            while (temp != 0) {
+                count++;
+                temp >>= 1;
+            }
+            
+            temp = n;
+        
+            for (int i=0; i<count; i++) {
+                int powerOfTwo = pow(2, i);
+                
+                if (!(temp & 1)) {
+                    n = n | powerOfTwo;
+                }
+                else {
+                    n = n ^ powerOfTwo;
+                }
+                
+                temp = temp >> 1;
+            }
+            return n;
+        }
+    }
+};
