d998010f0db97eee7c349c8d56abf02d75d7d71e
Abhinandan Wadhwa <abhinandanwadhwa5@gmail.com>
2022-09-14 10:39:14 +0530

    Two Pointer Approach

--- CodeStudio/IntersectionOfTwoArrays.cpp
+++ CodeStudio/IntersectionOfTwoArrays.cpp
@@ -1,6 +1,7 @@
 // https://www.codingninjas.com/codestudio/problems/intersection-of-2-arrays_1082149?source=youtube&campaign=love_babbar_codestudio1&utm_source=youtube&utm_medium=affiliate&utm_campaign=love_babbar_codestudio1&leftPanelTab=1
 
 
+
 vector<int> findArrayIntersection(vector<int> &arr1, int n, vector<int> &arr2, int m)
 {
 	int i = 0, j = 0;
