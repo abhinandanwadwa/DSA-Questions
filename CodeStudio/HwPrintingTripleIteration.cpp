18c2c3b9caae11ae34e6adb271fede247395ad22
Abhinandan Wadhwa <abhinandanwadhwa5@gmail.com>
2022-09-14 11:14:04 +0530

    More detailed Homework

--- CodeStudio/HwPrintingTripleIteration.cpp
+++ CodeStudio/HwPrintingTripleIteration.cpp
@@ -18,15 +18,19 @@ int main() {
 
     for (int i = 0; i < n; i++)
     {
+        cout << "This is loop 1" << endl;
         for (int j = i+1; j < n; j++)
         {
+            cout << "This is loop 2" << endl;
             for (int k = j+1; k < n; k++)
             {
+                cout << "This is loop 3" << endl;
                 cout << i << " " << j << " " << k << endl;
+                cout << "Loop 3 ending" << endl;
             }
-            
+            cout << "Loop 2 ending" << endl;
         }
-        
+       cout << "Loop 1 ending" << endl; 
     }
     
     
