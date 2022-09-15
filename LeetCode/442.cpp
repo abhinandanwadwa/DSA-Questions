41ba70aebfb80f325a4909c1b2f9514ae47e2b46
Abhinandan Wadhwa <abhinandanwadhwa5@gmail.com>
2022-09-15 09:10:56 +0530

    Homework: Find all duplicates in an array

--- LeetCode/442.cpp
+++ LeetCode/442.cpp
@@ -1,3 +1,32 @@
 // https://leetcode.com/problems/find-all-duplicates-in-an-array/
 
-// Homework!!
+
+
+class Solution {
+public:
+    vector<int> findDuplicates(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> ans;
+        
+        sort(nums.begin(), nums.end());
+    
+        
+        for (int i=0; i<n-1; i++) {
+            if (i == 0) {
+                if (nums[0] == nums[1]) {
+                    ans.push_back(nums[0]);
+                }
+            }
+            else if (i != n-1) {
+                if (nums[i] == nums[i+1] && nums[i] != nums[i-1]) {
+                    ans.push_back(nums[i]);
+                }
+            }
+        }
+        
+        
+        return ans;
+        
+            
+    }
+};
