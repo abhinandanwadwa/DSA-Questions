932a41ed948ee7efefc0081636a294362ffaec31
Abhinandan Wadhwa <abhinandanwadhwa5@gmail.com>
2022-10-01 17:34:38 +0530

    Move Zeroes (With Optimized Space Complexity)

--- LeetCode/283.cpp
+++ LeetCode/283.cpp
@@ -2,21 +2,13 @@ class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
         int n = nums.size();
-        int k = 0;
-        vector<int> ans;
-        int zerosCount = 0;
-        for (int i=0; i<n; i++) {
-            if (nums[i] != 0) {
-                ans.push_back(nums[i]);
-            }
-            else {
-                zerosCount++;
-            }
-        }
+        int i = 0;
 
-        for (int i=0; i<zerosCount; i++) {
-            ans.push_back(0);
+        for(int j=0; j<n; j++) {
+            if (nums[j] != 0) {
+                swap(nums[j], nums[i]);
+                i++;
+            }
         }
-        nums = ans;
     }
 };
