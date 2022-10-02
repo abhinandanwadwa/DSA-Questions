a7333e0cca341595fc613c80ed05d8a9b045a706
Abhinandan Wadhwa <abhinandanwadhwa5@gmail.com>
2022-10-02 12:25:18 +0530

    Rotate Array (Alternate)

--- LeetCode/189.cpp
+++ LeetCode/189.cpp
@@ -2,20 +2,10 @@ class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
         int n = nums.size();
-        if (n == 1) {
-            return;
+        vector<int> temp(n);
+        for (int i=0; i<n; i++) {
+            temp[(i+k)%n] = nums[i];
         }
-        while (k > n) {
-            k = k-n;
-        }
-        vector<int> ans;
-        for (int i=n-k; i<n; i++) {
-            ans.push_back(nums[i]);
-        }
-        for (int i=0; i<n-k; i++) {
-            ans.push_back(nums[i]);
-        }
-
-        nums = ans;
+        nums = temp;
     }
 };
