710fea14aef8d8e20b4f842dee2bd18228c60c87
Abhinandan Wadhwa <abhinandanwadhwa5@gmail.com>
2022-10-08 15:38:08 +0530

    Reverse String

---
+++ LeetCode/344.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        int n = s.size();
+        int st = 0, en = n-1;
+        while (st <= en) {
+            swap(s[st], s[en]);
+            st++;
+            en--;
+        }
+    }
+};
