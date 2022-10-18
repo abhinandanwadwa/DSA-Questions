a975ea18f7779d51e5d71055beccc6f219bd0b90
Abhinandan Wadhwa <abhinandanwadhwa5@gmail.com>
2022-10-18 16:11:11 +0530

    Rotate Image

---
+++ LeetCode/48.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        // STEP 1: Reverse the matrix upside down
+        int rows = n;
+        for (int i=0; i<rows/2; i++) {
+            for (int j=0; j<n; j++) {
+                swap(matrix[i][j], matrix[n-i-1][j]);
+            }
+        }
+
+        // STEP 2: 
+        for (int i=0; i<n; i++) {
+            for (int j=i; j<n; j++) {
+                swap(matrix[i][j], matrix[j][i]);
+            }
+        }
+    }
+};
